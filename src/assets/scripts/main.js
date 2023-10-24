76f571ef5617f88bce3747735fc0383c6b5eb7f2
Roser Fuster Bertomeu <rfusterb@uoc.edu>
2023-10-24 13:22:40 +0200

    dependència axe-core i correccions accessibilitat corresponents

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -9,8 +9,13 @@
 /**
  * Write any other JavaScript below
  */
-
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+axe.run({
+  runOnly: ['wcag2a', 'wcag2aa', 'best-practice']
+},).then((results) => {
+  // Analitzar els resultats
+  if (results.violations.length > 0) {
+      console.error('Violations:', results.violations);
+  } else {
+      console.log('No se encontraron violaciones de accesibilidad.');
+  }
+});
